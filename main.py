b6426e2866564d4626f341668333bf46b9f52866
qilin199595-rgb <qilin199595@gmail.com>
2026-06-10 15:28:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -144,3 +144,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 def index():
     return FileResponse("static/index.html")
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
